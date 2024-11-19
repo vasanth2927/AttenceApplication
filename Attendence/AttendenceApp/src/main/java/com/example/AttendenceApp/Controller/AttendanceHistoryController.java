54a413acad69e39b49b4a24dc7d2e477670c3410
vasanth2927 <vasanthvasu2927@gmail.com>
2024-11-19 23:54:31 +0530

    feat:change cloud reppo

--- Attendence/AttendenceApp/src/main/java/com/example/AttendenceApp/Controller/AttendanceHistoryController.java
+++ Attendence/AttendenceApp/src/main/java/com/example/AttendenceApp/Controller/AttendanceHistoryController.java
@@ -38,6 +38,9 @@ public class AttendanceHistoryController {
        
         System.out.println("hiiii");
 
+                System.out.println("hiiii 1");
+
+
         // Check if username is null
         if (username == null) {
             System.out.println("Username is null. User may not be logged in.");
